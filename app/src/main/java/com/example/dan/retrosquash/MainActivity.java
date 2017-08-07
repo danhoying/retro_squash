5673396706e3af3858e78d3cbad37c78c01293dc
Dan Hoying <danhoying@gmail.com>
2017-08-07 16:00:09 -0700

    Set persistent Night Mode theme with no action bar

--- app/src/main/java/com/example/dan/retrosquash/MainActivity.java
+++ app/src/main/java/com/example/dan/retrosquash/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.dan.retrosquash;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        AppCompatDelegate.setDefaultNightMode(
+                AppCompatDelegate.MODE_NIGHT_YES);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
