33a2f6325fb6561909eaa789e732d6f1096ab558
Dan Hoying <danhoying@gmail.com>
2017-08-08 09:55:40 -0700

    Initialize variables

--- app/src/main/java/com/example/dan/retrosquash/MainActivity.java
+++ app/src/main/java/com/example/dan/retrosquash/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.dan.retrosquash;
 
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.view.Display;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,44 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(
                 AppCompatDelegate.MODE_NIGHT_YES);
     }
+
+    Canvas canvas;
+    SquashCourtView squashCourtView;
+
+    // Sound variables
+    private SoundPool soundPool;
+    int sample1 = -1;
+    int sample2 = -1;
+    int sample3 = -1;
+    int sample4 = -1;
+
+    // Get display details
+    Display display;
+    Point size;
+    int screenWidth;
+    int screenHeight;
+
+    // Game objects
+    int racketWidth;
+    int racketHeight;
+    Point racketPosition;
+    int ballWidth;
+    Point ballPosition;
+
+    // For ball and racket movement
+    boolean ballIsMovingLeft;
+    boolean ballIsMovingRight;
+    boolean ballIsMovingUp;
+    boolean ballIsMovingDown;
+    boolean racketIsMovingLeft;
+    boolean racketIsMovingRight;
+
+    // Stats
+    long lastFrameTime;
+    int fps;
+    int score;
+    int lives;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
